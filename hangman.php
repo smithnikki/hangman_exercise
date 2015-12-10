dafabe175cb8704db730025b0e38cf7955aae243
Nicole Smith <nicolelsmith@gmail.com>
2015-12-10 15:08:22 -0500

    works, just no hanging guy yet

--- hangman.php
+++ hangman.php
@@ -1,13 +1,21 @@
 <?php
 
 class Hangman {
-  private $remaining_wrong_guesses = 10;
+
+  const LIMIT = 10;
   private $word;
-  private $letters = [];
+  private $good_letters = [];
+  private $bad_letters = [];
+  private $drawer;
 
   public function __construct() {
     echo "Starting the game\n";
-    $this->word = file_get_contents("http://randomword.setgetgo.com/get.php?len=10");
+    $this->drawer = new HangmanDrawer();
+    $this->word = str_split(
+      strtolower(
+        file_get_contents("http://randomword.setgetgo.com/get.php")
+      )
+    );
     $this->printState();
     $this->listenForInput();
 
@@ -22,19 +30,50 @@ class Hangman {
   }
 
   private function printState() {
-    echo "Remaining wrong guesses: " . $this->remaining_wrong_guesses . "\n";
-    for($i = 0; $i < strlen($this->word) ; $i++) {
-      echo "_ ";
-    }
-    echo "\n";
+    echo "Remaining wrong guesses: " . (self::LIMIT - count($this->bad_letters)) . "\n";
+    echo "Bad guesses: [" . join(', ', $this->bad_letters) . "]\n";
+    $this->printWordInProgress();
+    echo "\n\n\n\n";
   }
 
   private function listenForInput() {
     echo "Please type a letter: ";
     $input = readline();
-    array_push ($this->letters, $input);
+    $this->handleInput($input);
     $this->listenForInput();
   }
+
+  private function handleInput($input) {
+    if (in_array($input, $this->word)) {
+      array_push($this->good_letters, $input);
+    } else {
+      array_push($this->bad_letters, $input);
+      if(count($this->bad_letters) >= self::LIMIT) {
+        echo "The word is: " . join("", $this->word) . "\n";
+        echo "Game over!\n";
+
+        exit(0);
+      }
+    }
+    $this->printState();
+  }
+
+  private function printWordInProgress() {
+    $game_won = true;
+    foreach($this->word as $letter) {
+      if(in_array($letter, $this->good_letters)) {
+        echo $letter . " ";
+      } else {
+        echo "_ ";
+        $game_won = false;
+      }
+    }
+    if ($game_won){
+      echo "\n Hooray!!\n";
+      exit(0);
+    }
+  }
+
 
 }
 
